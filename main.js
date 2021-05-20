dc6934bf7741d766644bbea1451b5faa476cfe24
discordcodingcamp <84488901+discordcodingcamp@users.noreply.github.com>
2021-05-20 05:14:52 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+$('#staff-tog').click(function(){
+  $('.staff-toggle').toggleClass('tog-hidden tog-open');
+  $('.main-toggle').toggleClass('tog-hide tog-show');
+});
+});
+
+$(document).ready(function() {
+$('#cabin-tog').click(function(){
+  $('.cabin-toggle').toggleClass('tog-hidden tog-open');
+  $('.main-toggle').toggleClass('tog-hide tog-show');
+});
+});
+
+$(document).ready(function() {
+$('#challenges-tog').click(function(){
+  $('.challenges-toggle').toggleClass('tog-hidden tog-open');
+  $('.main-toggle').toggleClass('tog-hide tog-show');
+});
+});
+
+$(document).ready(function() {
+$('#leader-tog').click(function(){
+  $('.leader-toggle').toggleClass('tog-hidden tog-open');
+  $('.main-toggle').toggleClass('tog-hide tog-show');
+});
+});
+
+$(document).ready(function() {
+$('#user-tog').click(function(){
+  $('.user-toggle').toggleClass('tog-none tog-block');
+  $('.main-toggle').toggleClass('tog-hide tog-show');
+});
+});
